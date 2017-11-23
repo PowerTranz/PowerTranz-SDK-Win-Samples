22470d2f1f0062e87b3ed31ec6831bd91c3f1c1e
IanElsinga <mail@elsinga.ca>
2017-11-23 18:11:57 -0400

    Initial commit

---
+++ PtzSdk.Win.Samples.Console/Program.cs
@@ -0,0 +1,69 @@
+﻿using PowerTranzSDK.DataModel;
+using PowerTranzSDK.Requests;
+using PtzSdk.Win.Samples.Library;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PtzSdk.Win.Samples.Console
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            try
+            {
+
+
+                var sdk = new PtzSdkLibrary();
+                sdk.ApplicationId = "9eb64949-2fbc-4ef0-947f-c148469d37bb"; //dedicated ApplicationId for PtzSdk.Win.Samples.Console
+                sdk.GatewayKey = Properties.Settings.Default.GatewayKey;
+                sdk.PowerTranzId = Properties.Settings.Default.PowerTranzId;
+                sdk.PowerTranzPassword = Properties.Settings.Default.PowerTranzPassword;
+                sdk.TerminalIdleMessage = "Gateway Systems";
+                sdk.TerminalAddress = "Miura 676";
+
+                sdk.ConnectBluetooth();
+
+                var request = new PtzPaymentAuth
+                {
+                    OrderId = "11111",
+                    TotalAmount = 14.51m,
+                    TipAmount = 1.56m,
+                    TaxAmount = 2.13m,
+                    OtherAmount = 1.99m,
+                    CurrencyCode = "840",
+                    Source = new PtzPaymentSource
+                    {
+                        CardPresent = true,
+                        CardEmvFallback = true,
+                        Contactless = false,
+                        CardPan = string.Empty,
+                        MaskedPan = "476173******0119",
+                        CardCvv = string.Empty,
+                        CardExpiration = "2212",
+                        EncryptedCardTrackData = "cd2920ada07667e69dbda8a23773ec06774e7ce1bc34bed7cf7eee9f1ce412804d754bbae555feb196e38e45d44e93836e230b3ff5161f3f1f7b51503f885f6df35a493faac1d5346653e6b0b80584c3a1765ed94245ac83f975e8419890de6a4c010659056a14799ec54efffa876a088e9fe8386a2bda19cb5760328ebbe041",
+                        Ksn = "000002027c6124e00019",
+                    },
+                    TerminalId = "01249102",
+                    TerminalCode = "MiuraM010",
+                };
+
+                sdk.Terminal.DidConnectTerminal += () => sdk.StartTransaction(request);
+
+                System.Console.WriteLine("Press any key to exit");
+                System.Console.ReadKey();
+
+            }
+            catch(Exception x)
+            {
+                System.Console.Write(x.ToString());
+            }
+
+        }
+
+    }
+}
---
+++ PtzSdk.Win.Samples.Library/PtzSdkLibrary.cs
@@ -0,0 +1,314 @@
+﻿using Newtonsoft.Json;
+using PowerTranzSDK;
+using PowerTranzSDK.CardTerminals.Miura;
+using PowerTranzSDK.Miura.Commands.Miura;
+using PowerTranzSDK.Miura.Utilities;
+using PowerTranzSDK.Requests;
+using PowerTranzSDK.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PtzSdk.Win.Samples.Library
+{
+    public class PtzSdkLibrary
+    {
+
+        public PTZMiuraTerminal Terminal;
+        private PtzApi PtzApi;
+
+        public string ApplicationId { get; set; }
+        public string GatewayKey { get; set; }
+        public string PowerTranzId { get; set; }
+        public string PowerTranzPassword { get; set; }
+
+        private bool terminalConnected { get; set; } = false;
+
+        public bool IsContactless { get; set; } = false;
+
+        public string CurrentTransactionId { get; set; }    //Keeps track of the current transactionId for TransMod.
+        public string TerminalIdleMessage { get; set; } = "";
+        public bool IsActiveTransaction { get; set; } = false;
+        public string TerminalAddress { get; set; }
+
+        public PtzSdkLibrary()
+        {
+        }
+
+        #region "Connection"
+
+
+        public void ConnectBluetooth()
+        {
+            try
+            {
+                PtzApi = new PtzApi(ApplicationId, GatewayKey, PowerTranzId, PowerTranzPassword, false, 20);
+
+                Terminal = new PTZMiuraTerminal(PtzApi, 20);
+
+                RegisterListeners(Terminal);
+
+                var tsk = Terminal.ConnectTerminalWithInputTypeAsync(Enumerations.CardTerminalInputType.CardTerminalTypeBluetooth, TerminalAddress);
+
+                CommonUtility.LogInfo("Connecting via BlueTooth");
+            }
+            catch (Exception x)
+            {
+                CommonUtility.LogInfo($"Connection exception \n\n{x.ToString()}");
+            }
+
+        }
+
+
+        public void Disconnect()
+        {
+            Terminal.DisconnectTerminal();
+        }
+
+        #endregion
+
+        public void StartTransaction(PtzPaymentRequest request)
+        {
+            try
+            {
+                if (terminalConnected)
+                {
+                    if (IsContactless)
+                    {
+                        Task.Run(() => Terminal.BeginNFCTransactionWithRequest(request)); //Launch the transaction on another thread
+                    }
+                    else
+                    {
+                        Task.Run(() => Terminal.BeginEMVTransactionWithRequest(request));
+                    }
+
+                    CommonUtility.LogInfo("Transaction in progress");
+                }
+            }
+            catch (Exception x)
+            {
+                CommonUtility.LogInfo($"StartTransaction exception \r\n{x.ToString()}");
+            }
+        }
+
+        public void CancelTransaction()
+        {
+            if (terminalConnected) Terminal.CancelTransaction();
+        }
+
+        public void HardReset()
+        {
+            if (terminalConnected) Terminal.Driver.HardResetDevice();
+        }
+
+        public void SoftReset()
+        {
+            if (terminalConnected) Terminal.Driver.SoftResetDevice();
+        }
+
+
+
+        public void ClearScreen()
+        {
+            if (terminalConnected) Terminal.DisplayText(TerminalIdleMessage);
+        }
+        public List<PtzTransactionResponse> SearchTransactions(DateTime startDate, DateTime endDate, bool approved)
+        {
+
+            var ptzapi = new PtzApi(ApplicationId, GatewayKey, PowerTranzId, PowerTranzPassword, false, 20);
+
+            var req = new PtzTransactionRequest();
+            req.StartDateTime = startDate;
+            if (endDate != null) req.EndDateTime = endDate;
+
+            //var parameters = new Dictionary<string, string>();
+            //parameters.Add("StartDateTime", startDate.ToString());
+
+            req.Approved = approved;
+
+
+            var trxns = ptzapi.TransactionSearch(req);
+            return trxns.Transactions.ToList();
+        }
+
+
+        #region EventHandlers
+
+        private void RegisterListeners(PTZMiuraTerminal terminal)
+        {
+            terminal.DidConnectTerminal += DidConnectTerminalHandler;
+            terminal.DidDisconnectTerminal += DidDisconnectTerminalHandler;
+            terminal.DidFailToConnectTerminal += Terminal_DidFailToConnectTerminal; ;
+            terminal.WillConnectTerminal += WillConnectTerminalHandler;
+            terminal.DidFailWithError += Terminal_DidFailWithError;
+            terminal.DidFinishTransactionWithResponse += DidFinishTransactionWithResponseHandler;
+            terminal.TerminalPaymentFailed += TerminalPaymentFailedHandler;
+            terminal.DidRequestAccountTypeSelection += DidRequestAccountTypeSelectionHandler;
+            terminal.DidReturnAmountConfirmation += DidReturnAmountConfirmationHandler;
+            terminal.OnBatteryLow += OnBatteryLowHandler;
+            terminal.TerminalDidRequestTransactionAmount += TerminalDidRequestTransactionAmountHandler;
+            terminal.DidRequestSelectEMVApplication += DidRequestSelectEMVApplicationHandler;
+            terminal.TerminalDidRequestFinalConfirmation += TerminalDidRequestFinalConfirmationHandler;
+            terminal.DidRequestDevicePromptText += DidRequestDevicePromptTextHandler;
+            terminal.DidReceiveCardEvent += DidReceiveCardEventHandler;
+            terminal.SelectDevice += SelectDeviceHandler;
+            terminal.TerminalDidReceiveKeypadInput += TerminalDidReceiveKeypadInputHandler;
+
+            terminal.PromptCanceled += TerminalOnPromptCanceled;
+            terminal.DidFailWithReversal += Terminal_DidFailWithReversal;
+        }
+
+        private void Terminal_DidFailWithReversal(PtzPaymentResponse response, Enumerations.ReversalReason reason)
+        {
+            var responseString = "TRANSACTION FAILED\r\n\r\n" + JsonConvert.SerializeObject(response, Formatting.Indented);
+            responseString = responseString + $"\r\n Reversal reason {reason}";
+
+            CommonUtility.LogInfo(responseString);
+            CurrentTransactionId = response.TransactionIdentifier;
+
+        }
+
+        private void Terminal_DidFailToConnectTerminal(Exception error)
+        {
+            CommonUtility.LogInfo($"Terminal_DidFailToConnectTerminal- {error.ToString()}\r\n");
+            terminalConnected = false;
+        }
+
+        private void Terminal_DidFailWithError(Exception error)
+        {
+            try
+            {
+                CommonUtility.LogInfo($"Terminal_DidFailWithError - {error.ToString()}\r\n");
+                Terminal.DisplayText(TerminalIdleMessage);
+                IsActiveTransaction = false;
+            }
+            catch (Exception x)
+            {
+                CommonUtility.LogInfo($"{x.ToString()}\r\n");
+            }
+
+        }
+
+        private void TerminalOnPromptCanceled()
+        {
+            CommonUtility.LogInfo($"User canceled transaction\r\n");
+            IsActiveTransaction = false;
+            Terminal.DisplayText(TerminalIdleMessage);
+        }
+
+
+        private void OnReturnEncryptionStatus(CommandEnums.EncryptionStatus encryptionStatus)
+        {
+            CommonUtility.LogInfo($"OnReturnEncryptionStatus- {encryptionStatus.ToString()}\r\n");
+        }
+
+        private void DidConnectTerminalHandler()
+        {
+            CommonUtility.LogInfo($"CONNECTED");
+            terminalConnected = true;
+            Terminal.DisplayText(TerminalIdleMessage);
+
+        }
+
+        private void DidDisconnectTerminalHandler()
+        {
+            CommonUtility.LogInfo($"DISCONNECTED");
+            terminalConnected = false;
+            IsActiveTransaction = false;
+        }
+
+
+        private void WillConnectTerminalHandler()
+        {
+            CommonUtility.LogInfo($"Connecting...");
+        }
+
+
+        private void DidFinishTransactionWithResponseHandler(PtzPaymentResponse response)
+        {
+            try
+            {
+                IsActiveTransaction = false;
+
+                var responseString = JsonConvert.SerializeObject(response, Formatting.Indented);
+
+                CommonUtility.LogInfo($"Transaction complete\r\n{responseString}");
+
+                CurrentTransactionId = response.TransactionIdentifier;
+            }
+            catch (Exception x)
+            {
+                CommonUtility.LogInfo($"{x.ToString()}\r\n");
+            }
+
+        }
+
+        private void TerminalPaymentFailedHandler(PtzPaymentRequest request, string error)
+        {
+            try
+            {
+                CommonUtility.LogInfo($"Transaction Failed: reason - {error}\r\n");
+                Terminal.DisplayText("Declined");
+                IsActiveTransaction = false;
+            }
+            catch (Exception x)
+            {
+                CommonUtility.LogInfo($"{x.ToString()}\r\n");
+            }
+        }
+
+        private void DidRequestAccountTypeSelectionHandler(int[] accountType)
+        {
+            CommonUtility.LogInfo($"DidRequestAccountTypeSelectionHandler {accountType.ToString()}");
+        }
+
+        private void DidReturnAmountConfirmationHandler(bool confirmed)
+        {
+            CommonUtility.LogInfo($"DidRequestAccountTypeSelectionHandler {confirmed.ToString()}");
+        }
+
+        private void OnBatteryLowHandler(Enumerations.CardTerminalBatteryStatus batteryStatus)
+        {
+
+            switch (batteryStatus)
+            {
+                case Enumerations.CardTerminalBatteryStatus.CardTerminalBatteryStatusLow:
+                    CommonUtility.LogInfo($"BATTERY LOW");
+                    break;
+                case Enumerations.CardTerminalBatteryStatus.CardTerminalBatteryStatusCriticallyLow:
+                    CommonUtility.LogInfo($"BATTERY CRITICALLY LOW");
+                    break;
+            }
+
+        }
+
+        private void TerminalDidRequestTransactionAmountHandler()
+        { }
+
+        private void DidRequestSelectEMVApplicationHandler(List<string> applications)
+        { }
+
+        private void TerminalDidRequestFinalConfirmationHandler()
+        { }
+
+        private void DidRequestDevicePromptTextHandler(CommandEnums.DevicePrompt prompt)
+        { }
+
+        private void DidReceiveCardEventHandler(Enumerations.CardEvent eventType)
+        {
+            CommonUtility.LogInfo($"DidReceiveCardEventHandler {eventType.ToString()}");
+
+        }
+
+        private void SelectDeviceHandler(List<string> devices)
+        { }
+
+        private void TerminalDidReceiveKeypadInputHandler()
+        { }
+
+        #endregion
+
+    }
+}
